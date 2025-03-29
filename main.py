68e5bc6a72718a3034f9638828e38d18cf20fb2c
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2025-03-29 14:38:14 -0300

    OOP refactoring
    Massive refactoring in OOP form for making the code much cleaner and scalable for a future UI implementation
    
    Regex recognition has been removed for now. Will likely re-add it in the future

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 from random import randrange #Required for randomizing machine choice
-from choiceValTrtFuncs import *
-from roundExecFuncs import *
 
-# Choices are stored here ('-1' for empty)
-player_choice = -1 #user choice
-bot_choice = -1 #machine choice
+from classes.round import Round
+from functions.choice_functions import *
 
 ##### Executes from here
 while(True):
-	# Asks for user to choose _a move
-	# If choice is invalid, it will ask once again untill user inputs a valid choice
-	while(player_choice==-1): player_choice = user_choice() #reminder that getChoice() already calls treatChoice() inside it (which already prints the user choice)
+	current_round = Round() #Creates a new round object
+
+	# Asks for user to choose a move
+	current_round.player_choice = prompt_user_choice()
+	print(f'Your choice: {get_choice_name_From_idx(current_round.player_choice)}')
+
 
 	#BOT CHOICE
-	print("Adversary choice: ")
-	bot_choice = randrange(1,4,1)
-	choice_echo(bot_choice) # prints the bot's choice
+	current_round.bot_choice = randrange(1,4,1)
+	print(f'Adversary choice: {get_choice_name_From_idx(current_round.bot_choice)}')
 
-	# EXECUTES ROUND AND ANNOUNCES RESULT
-	print(f'\n{do_round(player_choice,bot_choice).center(48)}') #With a line break
+	# EXECUTES ROUND
+	print("...".center(48)) # First, prints a few dots to show the match is being calculated
+	current_round.calc_result() #Calculates the round's result
+
+	#ANNOUNCES RESULT
+	print(f'\n{(current_round.get_result_str()).center(48)}')
 
 
 	#TODO: Store amount of wins/losses and show them in the end of each round
-
-
-	# RESETS CHOICES FOR NEXT ROUND
-	player_choice=-1
-	bot_choice=-1
